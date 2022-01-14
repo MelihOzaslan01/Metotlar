189d038ace0d8f618b451e84a982ca2a6b71babb
Osman Melih Özaslan <melihozaslan01@gmail.com>
2022-01-14 19:55:18 +0300

    metotlar dersi işlendi

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 3;
+int b = 4;
+
+Console.WriteLine(a + b);
+
+int sonuc = Topla(a, b);
+Console.WriteLine(sonuc);
+
+Metotlar ornek = new Metotlar();
+ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+
+
+int sonuc2 = ornek.ArttirveTopla(ref a, ref b);
+ornek.EkranaYazdir(Convert.ToString(sonuc2));
+ornek.EkranaYazdir(Convert.ToString(a + b));
+
+
+
+
+
+
+static int Topla(int deger1, int deger2)
+{
+    return (deger1 + deger2);
+
+}
+
+class Metotlar
+{
+
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+
+    }
+
+    public int ArttirveTopla(ref int deger1, ref int deger2)
+    {
+        deger1 += 1;
+        deger2 += 1;
+        return deger1 + deger2;
+
+    }
+}
